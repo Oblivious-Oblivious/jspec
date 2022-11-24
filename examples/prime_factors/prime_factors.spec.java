d564ad7ab521cc0521b81cda461a8fabebbc0766
Oblivious <thanos.papap42@gmail.com>
2022-11-24 17:03:24 +0200

    Fixed prime factors spec.

--- examples/prime_factors/prime_factors.spec.java
+++ examples/prime_factors/prime_factors.spec.java
@@ -10,17 +10,17 @@ class PrimeFactors extends SpecModule {
                 this.o = new FactorsClass();
             });
             
-            xit("finds the factors of 1", () -> {
+            it("finds the factors of 1", () -> {
                 ArrayList<Integer> f1 = this.o.factors_of(1);
-                assert_that(f1.get(0)).equals_to(1);
+                assert_that(f1.size()).equals_to(0);
             });
 
-            xit("finds the factors of 2", () -> {
+            it("finds the factors of 2", () -> {
                 ArrayList<Integer> f2 = this.o.factors_of(2);
                 assert_that(f2.get(0)).equals_to(2);
             });
 
-            xit("finds the factors of 3", () -> {
+            it("finds the factors of 3", () -> {
                 ArrayList<Integer> f3 = this.o.factors_of(3);
                 assert_that(f3.get(0)).equals_to(3);
             });
@@ -34,15 +34,8 @@ class PrimeFactors extends SpecModule {
 
             it("find test factor of 2*2*3*3*5*7*11*11*13", () -> {
                 ArrayList<Integer> f = this.o.factors_of(2*2*3*3*5*7*11*11*13);
-                assert_that(f.get(0)).equals_to(2);
-                assert_that(f.get(1)).equals_to(2);
-                assert_that(f.get(2)).equals_to(3);
-                assert_that(f.get(3)).equals_to(3);
-                assert_that(f.get(4)).equals_to(5);
-                assert_that(f.get(5)).equals_to(7);
-                assert_that(f.get(6)).equals_to(11);
-                assert_that(f.get(7)).equals_to(11);
-                assert_that(f.get(8)).equals_to(13);
+                ArrayList<Integer> expected = SpecStructures.arraylist(2,2,3,3,5,7,11,11,13);
+                assert_that(f).equals_to(expected);
             });
         });
     }
